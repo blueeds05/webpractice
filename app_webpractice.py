4e25bc442dbf2b02793747247f1dd7e57901a83e
blueeds05 <blueeds05@yahoo.com>
2025-07-09 10:16:13 +0800

    Initial commit

---
+++ app_webpractice.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, redirect, request, url_for
+import json
+
+app = Flask(__name__)
+
+app.config['FILE'] = None
+app.config['FILE_DATA'] = None
+
+@app.route('/')
+def index():
+    result = None
+    file_current = app.config['FILE'].filename if app.config['FILE'] else None
+    return render_template('index.html', result=result, file_name=file_current)
+
+@app.route('/message/<message_id>', methods=['POST', 'GET'])
+def message(message_id):
+    error=None
+    message=None
+    jsfile = app.config['FILE']
+    jsfile_name = jsfile.filename
+    data = app.config['FILE_DATA']
+    try: 
+        if message_id in data:
+            message = data[message_id]  
+        else: 
+            raise KeyError
+    except KeyError:
+        error = "We do not have that message number. Please try again."
+    #import pdb;pdb.set_trace()
+    return render_template('index.html', file_name=jsfile_name, result=message, message_id=message_id, error=error)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    #import pdb;pdb.set_trace()
+    input_file = request.files.get('myfile_name')
+    #get file extension --- this can process json files only
+    file_ext = input_file.filename.split('.')[-1] if input_file else ''
+
+    #check if file is present and it must be json
+    #this can process json files only
+    if input_file:
+        if file_ext.lower() == 'json':
+            app.config['FILE'] = input_file
+            app.config['FILE_DATA'] = json.load(app.config['FILE'])
+        else:
+            error = "Chosen file is not a JSON file. Please try again"
+            return render_template('index.html',error=error,result=None)
+    
+    message_id = request.form['message_number']
+
+    return redirect(url_for('message',message_id=message_id))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
